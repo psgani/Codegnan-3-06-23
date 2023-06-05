02d47fe817fe2ddba097acba8fc1692c7fa7a961
psgani <131879872+psgani@users.noreply.github.com>
2023-06-05 12:37:06 +0530

    Add files via upload

---
+++ 3-06-23/javascript.js
@@ -0,0 +1,12 @@
+function progress(){
+    alert("The website is under development! Please bear with us while we add new features :)");
+}
+
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
